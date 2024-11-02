368b187dacddbf2bc124531f4ea79db12d433030
Aurel Lossou <aurelth@icloud.com>
2024-11-02 14:14:15 -0300

    MovimentacaoResponse criado

---
+++ Questao5/Questao5/Application/Commands/Responses/MovimentacaoResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Questao5.Application.Commands.Responses
+{
+    public class MovimentacaoResponse
+    {
+        public string IdMovimento { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
