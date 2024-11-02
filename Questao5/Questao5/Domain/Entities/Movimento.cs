677c00cd48a51fb556b28b720ae49e3675bad5e6
Aurel Lossou <aurelth@icloud.com>
2024-11-02 13:26:50 -0300

    Entidade Movimento criada

---
+++ Questao5/Questao5/Domain/Entities/Movimento.cs
@@ -0,0 +1,11 @@
+﻿namespace Questao5.Domain.Entities
+{
+    public class Movimento
+    {
+        public string IdMovimento { get; set; }
+        public string IdContaCorrente { get; set; }
+        public DateTime DataMovimento { get; set; }
+        public string TipoMovimento { get; set; } // "C" para Crédito, "D" para Débito
+        public decimal Valor { get; set; }
+    }
+}
