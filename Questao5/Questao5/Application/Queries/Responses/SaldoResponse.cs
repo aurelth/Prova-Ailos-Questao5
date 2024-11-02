23c813f82a8c2760abd4a6489a212d62f50cb2ee
Aurel Lossou <aurelth@icloud.com>
2024-11-02 17:23:56 -0300

    SaldoRequest e SaldoResponse criados

---
+++ Questao5/Questao5/Application/Queries/Responses/SaldoResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Questao5.Application.Queries.Responses
+{
+    public class SaldoResponse
+    {
+        public string NumeroConta { get; set; }
+        public string NomeTitular { get; set; }
+        public decimal SaldoAtual { get; set; }
+        public DateTime DataHoraConsulta { get; set; }
+    }
+}
