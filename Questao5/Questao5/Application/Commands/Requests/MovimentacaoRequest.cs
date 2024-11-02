e6d81f28c8f86b670799d03c429a5336f2a42b6a
Aurel Lossou <aurelth@icloud.com>
2024-11-02 13:42:28 -0300

    Instalação do Pacotes Nugets e criação da classe MovimentacaoRequest

---
+++ Questao5/Questao5/Application/Commands/Requests/MovimentacaoRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Questao5.Application.Commands.Requests
+{
+    public class MovimentacaoRequest
+    {
+        public string IdRequisicao { get; set; }
+        public string IdContaCorrente { get; set; }
+        public decimal Valor { get; set; }
+        public string TipoMovimento { get; set; } // "C" para Crédito, "D" para Débito
+    }
+}
